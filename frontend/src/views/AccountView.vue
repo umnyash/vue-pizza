194c406cc17bb56ba1a07e74a32b9dcd973e731a
Rustam Sadreev <rustam17.sb@gmail.com>
2025-12-03 22:52:57 +0300

    feat: add AccountView component

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -1,3 +1,31 @@
 <template>
-  <router-view />
+  <app-sidebar>
+    <template #links>
+      <router-link
+        class="layout__link"
+        :class="{ 'layout__link--active': routeName === 'orders' }"
+        :to="{ name: 'orders' }"
+      >
+        История заказов
+      </router-link>
+      <router-link
+        class="layout__link"
+        :class="{ 'layout__link--active': routeName === 'profile' }"
+        :to="{ name: 'profile' }"
+      >
+        Мои данные
+      </router-link>
+    </template>
+
+    <router-view />
+  </app-sidebar>
 </template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import AppSidebar from "@/layouts/AppSidebar.vue";
+
+const route = useRoute();
+const routeName = computed(() => route.name);
+</script>
