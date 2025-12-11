6a1ecde9d581b584a80a3debd3cf2b00c342ab6c
Rustam Sadreev <rustam17.sb@gmail.com>
2025-12-11 03:59:36 +0300

    feat: replace CartPizzasList content with dynamic pizzas data

--- frontend/src/stores/cart.js
+++ frontend/src/stores/cart.js
@@ -1,10 +1,25 @@
 import { defineStore } from "pinia";
+import { useDataStore } from "@/stores";
 
 export const useCartStore = defineStore("cart", {
   state: () => ({
     pizzas: [],
   }),
-  getters: {},
+  getters: {
+    expandedPizzas(state) {
+      const dataStore = useDataStore();
+
+      return state.pizzas.map((pizza) => ({
+        ...pizza,
+        dough: dataStore.getDoughById(pizza.doughId),
+        sauce: dataStore.getSauceById(pizza.sauceId),
+        size: dataStore.getSizeById(pizza.sizeId),
+        ingredients: Object.keys(pizza.ingredientsCounts).map((ingredientId) =>
+          dataStore.getIngredientById(+ingredientId),
+        ),
+      }));
+    },
+  },
   actions: {
     addPizza(pizza) {
       this.pizzas.push(pizza);
